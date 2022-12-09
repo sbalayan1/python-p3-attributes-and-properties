ae1c8cf7c697d0efd53d8cf006da358c35fd6069
sbalayan1 <balayans2014@yahoo.com>
2022-12-08 17:39:44 -0800

    complete

--- lib/person.py
+++ lib/person.py
@@ -2,3 +2,28 @@
 
 class Person:
     pass
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        pass
+        if (type(name) == str) and (1< len(name) < 25):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        pass
+        appr_jobs = ["ITC"]
+        if(job in appr_jobs):
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
